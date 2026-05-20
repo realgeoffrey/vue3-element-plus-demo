2fff4e8149e418dcdefa2edd59af10c8a68bb5b4
feijie.zheng <feijie.zheng@lkcoffee.com>
2026-05-20 11:58:49 +0800

    1

--- src/views/JsxView.vue
+++ src/views/JsxView.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import Jsx from '@/components/jsx/JsxFather.vue'
 </script>
 
--- src/views/PaginationView.vue
+++ src/views/PaginationView.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import Pagination from '@/components/pagination/index.vue'
 </script>
 
